064bf7e1b76ad48fc42c81c6ab2e8daed2e0102c
Daniel Basman <dbasman2@gmail.com>
2019-12-16 09:42:44 -0800

    Initial Commit

---
+++ FirstAppNoSwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstAppNoSwiftUI
+//
+//  Created by Daniel Basman on 12/16/19.
+//  Copyright © 2019 Daniel Basman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
